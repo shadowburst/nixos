da33e5a683bf48b9afc5353f515abea2bd630f9a
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2022-10-14 13:40:18 +0200

    Launch emacs script on rebuild

--- modules/programs/emacs/default.nix
+++ modules/programs/emacs/default.nix
@@ -6,24 +6,24 @@
   };
 
   home.packages = with pkgs; [
+    coreutils
     fd
     ripgrep
   ];
 
+  system.userActivationScipts.emacs.text = ''
+    DOOM="$HOME/.emacs.d"
+
+    if [ ! -d "$DOOM" ]; then
+        git clone https://github.com/hlissner/doom-emacs.git $DOOM
+        "$DOOM/bin/doom" -y install
+    fi
+
+    "$DOOM/bin/doom" sync
+  '';
   
   xdg.configFile."doom" = {
     source = ./config;
     recursive = true;
-    onChange = ''
-      #!/usr/bin/env bash
-
-      DOOM="$HOME/.emacs.d"
-
-      if [ ! -d "$DOOM" ]; then
-          git clone https://github.com/hlissner/doom-emacs.git $DOOM
-          $DOOM/bin/doom -y install
-      fi
-      $DOOM/bin/doom sync
-    '';
   };
 }
