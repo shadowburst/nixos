a90c0b27f78087fb3681a42c51a2182165a89996
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2023-09-08 23:07:14 +0200

    feat(eww): use system colors

--- config/eww/default.nix
+++ config/eww/default.nix
@@ -4,45 +4,47 @@ let
   theme = import ../../theme;
 in
 {
-  programs.eww = {
-    enable = true;
-    package = pkgs.eww-wayland;
-    configDir = ./config;
+  home.packages = with pkgs; [
+    eww-wayland
+  ];
+
+  xdg.configFile."eww" = {
+    source = ./config;
+    recursive = true;
   };
 
-  # xdg.configFile = with theme; {
-  #   "eww/common/colors.scss".text = ''
-  #     $foreground: ${colors.fg}
-  #     $background: ${colors.bg};
-  #     $card: ${colors.card};
-  #     $primary: ${colors.primary};
-  #     $secondary: ${colors.secondary};
-  #     $success: ${colors.success};
-  #     $warning: ${colors.warning};
-  #     $danger: ${colors.danger};
-  #     $disabled: ${colors.disabled};
-  #     $colors: (
-  #         foreground: $foreground,
-  #         background: $background,
-  #         card: $card,
-  #         primary: $primary,
-  #         secondary: $secondary,
-  #         success: $success,
-  #         warning: $warning,
-  #         danger: $danger,
-  #         disabled: $disabled,
-  #         transparent: transparent,
-  #     );
-  #   '';
-  #   "eww/common/fonts.scss".text = ''
-  #     $normal-font: "${fonts.normal}";
-  #     $mono-font: "${fonts.mono}";
-  #     $nerd-font: "${fonts.nerdMono}";
-  #     $fonts: (
-  #         normal: $normal-font,
-  #         mono: $mono-font,
-  #         nerd: $nerd-font,
-  #     );
-  #   '';
-  # };
+  xdg.configFile."eww/common/colors.scss".text = with theme; ''
+    $foreground: ${colors.fg};
+    $background: ${colors.bg};
+    $card: ${colors.card};
+    $primary: ${colors.primary};
+    $secondary: ${colors.secondary};
+    $success: ${colors.success};
+    $warning: ${colors.warning};
+    $danger: ${colors.danger};
+    $disabled: ${colors.disabled};
+    $colors: (
+        foreground: $foreground,
+        background: $background,
+        card: $card,
+        primary: $primary,
+        secondary: $secondary,
+        success: $success,
+        warning: $warning,
+        danger: $danger,
+        disabled: $disabled,
+        transparent: transparent,
+    );
+  '';
+
+  xdg.configFile."eww/common/fonts.scss".text = with theme; ''
+    $normal-font: "${fonts.normal}";
+    $mono-font: "${fonts.mono}";
+    $nerd-font: "${fonts.nerdMono}";
+    $fonts: (
+        normal: $normal-font,
+        mono: $mono-font,
+        nerd: $nerd-font,
+    );
+  '';
 }
